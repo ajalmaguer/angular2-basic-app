3a1b1a8c818cf28c635e98cc3e51ef08b7e6a6cf
AJ Almaguer <ajalmaguer@gmail.com>
2016-09-27 02:01:30 -0700

    updated navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,16 +13,15 @@ export class NavbarComponent implements OnInit {
 	constructor(private router: Router) { }
 
 	ngOnInit() {
-		$(".button-collapse").sideNav();
-		$(".dropdown-button").dropdown();
-		$('.collapsible').collapsible({
-			accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+		$(".button-collapse").sideNav({
+			closeOnClick: true
 		});
+		$(".dropdown-button").dropdown();
+		// $('.collapsible').collapsible({
+		// 	accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+		// });
 	}
 
-	goTo(page) {
-		this.router.navigate(page)
-		$(".button-collapse").sideNav('hide');
-	}
+	
 
 }
