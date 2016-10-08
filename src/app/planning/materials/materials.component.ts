1fc710c71a99f89d5f1230b54a6a0fbc3b417179
AJ Almaguer <ajalmaguer@gmail.com>
2016-10-08 01:29:23 -0700

    finished planning section

--- src/app/planning/materials/materials.component.ts
+++ src/app/planning/materials/materials.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-materials',
-  templateUrl: 'materials.component.html',
-  styleUrls: ['materials.component.css']
+	selector: 'app-materials',
+	templateUrl: 'materials.component.html',
+	styleUrls: ['materials.component.css']
 })
 export class MaterialsComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
